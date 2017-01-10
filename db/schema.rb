1f7703cc408c37e4bcadceb80f23ebc2fd080b06
Gustavo Santiago <gustavosantig1@gmail.com>
2017-01-10 02:20:31 -0300

    inserção de anexo de fotos nos tweets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110022142) do
+ActiveRecord::Schema.define(version: 20170110051740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,12 @@ ActiveRecord::Schema.define(version: 20170110022142) do
   create_table "tweets", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "user_profiles", force: :cascade do |t|
