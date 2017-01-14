82f03560dbd37b71006cc71fc96b4b40fc8f65e9
Gustavo Santiago <gustavosantig1@gmail.com>
2017-01-14 03:52:02 -0300

    revisão de telas do devise e tratamento de imagens do usuário

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -4,6 +4,6 @@ class UserProfile < ApplicationRecord
   validates :name, presence: true
   validates :bio, length: { minimum: 10, maximum: 120 }
 
-  has_attached_file :avatar, styles: { thumb: "100x100>", medium: "175x170>" }
+  has_attached_file :avatar, styles: { thumb: "100x100>", medium: "300x300>" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
